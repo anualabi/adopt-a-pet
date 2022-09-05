2859db15353ff1c6b66a038dd496957e32799f02
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2022-09-05 17:11:15 +0200

    update css variables in global styles

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -3,7 +3,9 @@ import { createGlobalStyle, css } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`${css`
   :root {
     --background: hsl(207, 26%, 17%);
-    --text: hsl(0, 0%, 100%);
+    --text-color: hsl(0, 0%, 100%);
+    --bold-font: 700;
+    --normal-font: 400;
   }
   body {
     margin: 0;
--- src/app/components/NavBar/NavbarStyles.ts
+++ src/app/components/NavBar/NavbarStyles.ts
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 
 export const StyledNavbar = styled.nav`
   background-color: var(--background);
-  color: var(--text);
+  color: var(--text-color);
+  margin-bottom: 2rem;
+  width: 100vw;
 
   .logo-wrapper {
     h1 {
       font-size: 2rem;
+      font-weight: var(--bold-font);
       letter-spacing: 1px;
       padding: 1rem 0;
       margin: 0;
