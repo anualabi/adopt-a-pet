1b8501f947f8aa156527ddad7fddc0a9bf0b6516
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2022-09-08 22:54:43 +0200

    update global styles

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -4,11 +4,16 @@ export const GlobalStyles = createGlobalStyle`${css`
   :root {
     --background: hsl(207, 26%, 17%);
     --text-color: hsl(0, 0%, 100%);
+    --element: hsl(209, 23%, 22%);
     --bold-font: 700;
     --normal-font: 400;
   }
   body {
     margin: 0;
     font-family: 'Roboto', sans-serif;
+  }
+  button {
+    background: var(--element);
+    color: var(--text-color);
   }
 `}`;
